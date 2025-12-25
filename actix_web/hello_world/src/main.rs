730638ba65c7659022d6e6c28537ec1df85caf38
solwarrior <118261237+sol-warrior@users.noreply.github.com>
2025-12-25 22:35:54 +0530

    Refactor Actix Web application to implement a RESTful API
    Updated the main application to include multiple endpoints for handling HTTP requests. Added GET and POST routes for greeting, echoing input, and personalized responses. The server is now structured to serve requests under a scoped path, enhancing organization and maintainability. Updated dependencies in Cargo.toml to use actix-web version 4.12.1.

--- actix_web/hello_world/src/main.rs
+++ actix_web/hello_world/src/main.rs
@@ -1,3 +1,60 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, post, web};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
 }
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+#[post("/name")]
+async fn name(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(format!("Hi {}", req_body))
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+async fn hi() -> impl Responder {
+    HttpResponse::Ok().body("You are warrior! Never give up.")
+}
+
+async fn index() -> impl Responder {
+    "Hello world!"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(
+            // prefixes all resources and routes attached to it...
+            web::scope("/app")
+                // ...so this handles requests for `GET /app/index.html`
+                .service(hello)
+                .route("/index.html", web::get().to(index))
+                .route("hi", web::get().to(hi))
+                .route("/hey", web::get().to(manual_hello)),
+        )
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
+// #[actix_web::main]
+// async fn main() -> std::io::Result<()> {
+//     HttpServer::new(|| {
+//         App::new()
+//             .service(hello)
+//             .service(echo)
+//             .service(name)
+//             .route("hi", web::get().to(hi))
+//             .route("/hey", web::get().to(manual_hello))
+//     })
+//     .bind(("127.0.0.1", 8080))?
+//     .run()
+//     .await
+// }
