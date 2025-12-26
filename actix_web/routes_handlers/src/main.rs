f2a0cded5426b7322b851c67b0956ffbdc04a34b
solwarrior <118261237+sol-warrior@users.noreply.github.com>
2025-12-26 13:25:41 +0530

    learned return json body and how to request json body
    Added a POST route for adding two numbers, along with a corresponding request and response structure using Serde for JSON handling. Updated the main function to include the new endpoint and improved the existing greeting and sum endpoints to return HTTP responses. This refactor improves the API's functionality and organization.

--- actix_web/routes_handlers/src/main.rs
+++ actix_web/routes_handlers/src/main.rs
@@ -1,35 +1,74 @@
-use actix_web::{App, HttpServer, Responder, get, web};
-use log::info;
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, post, web};
+use serde::{Deserialize, Serialize};
 
-async fn greet(path: web::Path<String>) -> impl Responder {
-    let name = path.into_inner();
-    format!("Hello, {}!", name)
+#[derive(Deserialize)]
+struct Add_Request_Body {
+    a: u32,
+    b: u32,
 }
 
-async fn sum2(path: web::Path<(u32, u32)>) -> impl Responder {
-    let (first, second) = path.into_inner();
-
-    format!("sum : {}", first + second)
+#[derive(Serialize)]
+struct Add_Response_Body {
+    sum: u32,
+}
+#[get("/hello/{name}")]
+async fn greet(path: web::Path<String>) -> impl Responder {
+    let name = path.into_inner();
+    HttpResponse::Ok().body(format!("Hello, {}!", name))
 }
 
 #[get("/sum/{first}/{second}")]
 async fn sum(path: web::Path<(u32, u32)>) -> impl Responder {
-    let (first, second) = path.into_inner();
-    format!("sum of {} + {} = {}", first, second, first + second)
+    let (a, b) = path.into_inner();
+    HttpResponse::Ok().body(format!("sum: {}", a + b))
 }
 
+#[post("/add")]
+async fn add(req_body: web::Json<Add_Request_Body>) -> impl Responder {
+    let a = req_body.a;
+    let b = req_body.b;
+
+    let add = a + b;
+    HttpResponse::Ok().json(Add_Response_Body { sum: add })
+}
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::init();
-    HttpServer::new(|| {
-        App::new()
-            .service(sum)
-            .route("/hello/{name}", web::get().to(greet))
-            .route("/sum2/{first}/{second}", web::get().to(sum2))
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(greet).service(sum).service(add))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
 
-// GET /sum/5/7 → return “sum: 12”
+// use actix_web::{App, HttpServer, Responder, get, web};
+// use log::info;
+
+// async fn greet(path: web::Path<String>) -> impl Responder {
+//     let name = path.into_inner();
+//     format!("Hello, {}!", name)
+// }
+
+// async fn sum2(path: web::Path<(u32, u32)>) -> impl Responder {
+//     let (first, second) = path.into_inner();
+
+//     format!("sum : {}", first + second)
+// }
+
+// #[get("/sum/{first}/{second}")]
+// async fn sum(path: web::Path<(u32, u32)>) -> impl Responder {
+//     let (first, second) = path.into_inner();
+//     format!("sum of {} + {} = {}", first, second, first + second)
+// }
+
+// #[actix_web::main]
+// async fn main() -> std::io::Result<()> {
+//     env_logger::init();
+//     HttpServer::new(|| {
+//         App::new()
+//             .service(sum)
+//             .route("/hello/{name}", web::get().to(greet))
+//             .route("/sum2/{first}/{second}", web::get().to(sum2))
+//     })
+//     .bind(("127.0.0.1", 8080))?
+//     .run()
+//     .await
+// }
